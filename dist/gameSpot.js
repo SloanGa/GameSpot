4bcbc7669794e3844eb1bb390729f0427bc7cd1e
Sloan Gauthier <sloangauthier@gmail.com>
2024-07-22 08:19:30 +0200

    build in js

--- dist/gameSpot.js
+++ dist/gameSpot.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = require("path");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
 const locals_middleware_1 = require("./middlewares/locals.middleware");
 const errors_middleware_1 = require("./middlewares/errors.middleware");
 const games_json_1 = __importDefault(require("./models/games.json"));
@@ -16,8 +19,8 @@ app.use(express_1.default.static((0, path_1.join)(__dirname.replace(/\\dist$/, "
 const locals = (0, locals_middleware_1.createLocals)(app, games_json_1.default);
 app.use(locals);
 app.use(routes_1.default);
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 const manageErrors = (0, errors_middleware_1.createManageErrors)(app);
 app.use(manageErrors);
